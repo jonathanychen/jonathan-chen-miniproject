fa7f4633b65f2a21ba6b6d115553736af8d0e770
Jonathan Chen <jonathanyangchen@gmail.com>
2025-09-11 02:44:25 -0400

    feat: write tests for Book and RouteController classes (#2)

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/BOOKUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/BOOKUnitTests.java
@@ -1,9 +1,19 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import dev.coms4156.project.individualproject.model.Book;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,8 +25,8 @@ public class BookUnitTests {
 
   public static Book book;
 
-  @BeforeAll
-  public static void setUpBookForTesting() {
+  @BeforeEach
+  public void setUpBookForTesting() {
     book = new Book("When Breath Becomes Air", 0);
   }
 
@@ -26,4 +36,199 @@ public class BookUnitTests {
     assertEquals(cmpBook, book);
   }
 
+  @Test
+  public void equalsDifferentBooksTest() {
+    Book cmpBook = new Book("Some other title", 15);
+    assertNotEquals(cmpBook, book);
+  }
+
+  @Test
+  public void hasCopiesTest() {
+    assertTrue(book.hasCopies());
+
+    book.deleteCopy();
+
+    assertFalse(book.hasCopies());
+  }
+
+  @Test
+  public void deleteCopyTest() {
+    assertTrue(book.deleteCopy());
+    assertFalse(book.deleteCopy());
+  }
+
+  @Test
+  public void hasMultipleAuthorsTest() {
+    assertFalse(book.hasMultipleAuthors());
+
+    book.setAuthors(new ArrayList<>(Arrays.asList("author1", "author2")));
+
+    assertTrue(book.hasMultipleAuthors());
+  }
+
+  @Test
+  public void setTitleTest() {
+    String newTitle = "Example title";
+
+    book.setTitle(newTitle);
+
+    assertEquals(newTitle, book.getTitle());
+  }
+
+  @Test
+  public void setAuthorsTest() {
+    ArrayList<String> authorsToSet = new ArrayList<>(Arrays.asList("author1", "author2"));
+    ArrayList<String> expected = new ArrayList<>(authorsToSet);
+
+    book.setAuthors(authorsToSet);
+
+    authorsToSet.remove(authorsToSet.size() - 1);
+
+    assertIterableEquals(expected, authorsToSet);
+  }
+
+  @Test
+  public void getAuthorsTest() {
+    ArrayList<String> authors = new ArrayList<>(Arrays.asList());
+
+    book.setAuthors(authors);
+
+    ArrayList<String> retrievedAuthors = book.getAuthors();
+
+    retrievedAuthors.add("some author");
+
+    assertIterableEquals(authors, book.getAuthors());
+  }
+
+  @Test
+  public void setLanguageTest() {
+    String newLanguage = "French";
+
+    book.setLanguage(newLanguage);
+
+    assertEquals(newLanguage, book.getLanguage());
+  }
+
+  @Test
+  public void setShelvingLocationTest() {
+    String shelvingLocation = "someLocation";
+
+    book.setShelvingLocation(shelvingLocation);
+
+    assertEquals(shelvingLocation, book.getShelvingLocation());
+  }
+
+  @Test
+  public void setPublicationDateTest() {
+    String publicationDate = "some date";
+
+    book.setPublicationDate(publicationDate);
+
+    assertEquals(publicationDate, book.getPublicationDate());
+  }
+
+  @Test
+  public void setPublisherTest() {
+    String publisher = "me";
+
+    book.setPublisher(publisher);
+
+    assertEquals(publisher, book.getPublisher());
+  }
+
+  @Test
+  public void setReturnDatesTest() {
+    ArrayList<String> returnDatesToSet = new ArrayList<>(Arrays.asList("date1", "date2"));
+    ArrayList<String> expected = new ArrayList<>(returnDatesToSet);
+
+    book.setReturnDates(returnDatesToSet);
+
+    returnDatesToSet.remove(returnDatesToSet.size() - 1);
+
+    assertIterableEquals(expected, returnDatesToSet);
+  }
+
+  @Test
+  public void getReturnDatesTest() {
+    ArrayList<String> returnDates = new ArrayList<>(Arrays.asList("date1"));
+    ArrayList<String> expected = new ArrayList<>(returnDates);
+
+    book.setReturnDates(returnDates);
+
+    ArrayList<String> retrievedReturnDates = book.getReturnDates();
+
+    retrievedReturnDates.add("some return date");
+
+    assertIterableEquals(expected, book.getReturnDates());
+  }
+
+  @Test
+  public void setSubjectsTest() {
+    ArrayList<String> subjects = new ArrayList<>(Arrays.asList("geography", "history", "coding"));
+    ArrayList<String> expected = new ArrayList<>(subjects);
+
+    book.setSubjects(subjects);
+
+    subjects.remove(subjects.size() - 1);
+
+    assertIterableEquals(expected, subjects);
+  }
+
+  @Test
+  public void getSubjectsTest() {
+    ArrayList<String> subjects = new ArrayList<>(Arrays.asList("math", "science"));
+    ArrayList<String> expected = new ArrayList<>(subjects);
+
+    book.setSubjects(subjects);
+
+    ArrayList<String> retrievedSubjects = book.getSubjects();
+
+    retrievedSubjects.add("gaming");
+
+    assertIterableEquals(expected, book.getSubjects());
+  }
+
+  @Test
+  public void setTotalCopiesTest() {
+    int totalCopies = 5;
+    book.setTotalCopies(totalCopies);
+
+    assertEquals(totalCopies, book.getTotalCopies());
+  }
+
+  @Test
+  public void toStringTest() {
+    assertEquals("(0)\tWhen Breath Becomes Air", book.toString());
+  }
+
+  @Test
+  public void checkoutCopyTest() {
+    assertEquals(0, book.getAmountOfTimesCheckedOut());
+    assertTrue(book.hasCopies());
+
+    assertNotNull(book.checkoutCopy());
+
+    assertEquals(1, book.getAmountOfTimesCheckedOut());
+    assertFalse(book.hasCopies());
+
+    assertNull(book.checkoutCopy());
+  }
+
+  @Test
+  public void returnCopyTest() {
+    book.setReturnDates(new ArrayList<>(Arrays.asList("some date")));
+
+    assertEquals(1, book.getReturnDates().size());
+    assertTrue(book.hasCopies());
+    assertEquals(1, book.getCopiesAvailable());
+
+    assertTrue(book.returnCopy("some date"));
+
+    assertEquals(0, book.getReturnDates().size());
+    assertFalse(book.hasCopies());
+    assertEquals(0, book.getCopiesAvailable());
+
+    assertFalse(book.returnCopy("some other date"));
+
+  }
 }
